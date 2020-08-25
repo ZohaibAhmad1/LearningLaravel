9975cdf74b7d47ac6f77fe1f505b29070c0e9f10
ZohaibAhmad1 <ahmadzohaib046@gmail.com>
2020-08-25 15:04:21 +0500

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+       <v-container>
+       <v-alert type="info"> zohaib1..44.Ahmade </v-alert>
+</v-container>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
